65d81c3a928e8a904679453a1deaeb44daf1180b
Nahid Hasan Noyon <nahidhasannoyon20@gmail.com>
2025-04-14 10:41:31 +0600

    Refactor SplashController and SplashPage for improved navigation and layout; update UI elements and styles

--- lib/app/modules/splash/splash_controller.dart
+++ lib/app/modules/splash/splash_controller.dart
@@ -2,8 +2,8 @@ import 'package:eco_meter/app/core/constants/app_imports.dart';
 
 class SplashController extends GetxController {
   @override
-  void onInit() {
-    super.onInit();
+  void onReady() {
+    super.onReady();
     // Navigate to the sign in page after a delay
     Future.delayed(const Duration(seconds: 2), () {
       Get.offNamed(SignInRoutes.signIn);
--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -5,20 +5,41 @@ class SplashPage extends GetView<SplashController> {
 
   @override
   Widget build(BuildContext context) {
+    Get.find<SplashController>();
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(AppImages.appLogo, width: 200, height: 200),
-            const SizedBox(height: 20),
-            Text(
-              'Powered by',
-              style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+      body: Flex(
+        direction: Axis.vertical,
+        children: [
+          Flexible(
+            flex: 13,
+            child: Center(
+              child: Image.asset(AppImages.appLogo, width: 200, height: 200),
             ),
-          ],
-        ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Powered by',
+                  style: TextStyle(fontSize: 12, color: Colors.grey),
+                ),
+                const SizedBox(width: 5),
+                Text(
+                  'M360 ICT',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
